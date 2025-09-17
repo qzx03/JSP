c4d4da6158bccce405ab589c7f16bb3c7d471fb8
ahmed <ahmedelmorabiti123460@gmail.com>
2025-09-17 11:48:59 +0100

    added ifs

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ let n1 = 10;
 let n2 = 5;
 let res = n1 + n2;
 console.log("the result is : " + res);
+
+
+let age = 20;
+if (age < 18) {
+    console.log("You are a minor");
+} else if (age === 18) {
+    console.log("You have just reached adulthood");
+} else {
+    console.log("You are an adult");
+}
