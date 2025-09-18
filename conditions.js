ab5b0c5903997d1148c7c7b71ff1cd5b85ac170b
Ahmed El Morabiti <ahmedelmorabiti123460@gmail.com>
2025-09-18 10:07:35 +0100

    Delete conditions.js

--- conditions.js
+++
@@ -1,8 +0,0 @@
-let age = 20;
-if (age < 18) {
-    console.log("You are a minor");
-} else if (age === 18) {
-    console.log("You have just reached adulthood");
-} else {
-    console.log("You are an adult");
-}
