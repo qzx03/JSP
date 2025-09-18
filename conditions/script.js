f1607bfff0d4eb27108afc220d10a509d0234c70
ahmed <ahmedelmorabiti123460@gmail.com>
2025-09-18 10:08:48 +0100

    final

---
+++ conditions/script.js
@@ -0,0 +1,8 @@
+let age = 20;
+if (age < 18) {
+    console.log("You are a minor");
+} else if (age === 18) {
+    console.log("You have just reached adulthood");
+} else {
+    console.log("You are an adult");
+}
---
+++ functions/script.js
@@ -0,0 +1,10 @@
+function addition(a,b) {
+    return a + b
+}
+function subtraction(a,b) {
+    return a - b
+}
+let res2 = addition(4,7);
+let res3 = subtraction(9,3);
+console.log("the addition is: " + res2);
+console.log("the subtraction is: " + res3);
